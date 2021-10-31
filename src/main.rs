2dbb31dc62f52392d5fc44602ab1a88c7eb51a2f
Lukas Widmer <luky.widmer@gmail.com>
2021-10-31 11:10:39 +0900

    added Yew Router

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use store::*;
 use yew::prelude::*;
+use yew_router::components::RouterAnchor;
+use yew_router::prelude::Router;
+use yew_router::*;
 use yewdux::prelude::*;
 use yewtil::NeqAssign;
 
@@ -7,6 +10,16 @@ mod page;
 mod page_list;
 mod store;
 mod sub_store;
+
+#[derive(Clone, Switch)]
+pub enum MainRoute {
+    #[to = "/page/{id}"]
+    Page(i32),
+    #[to = "/"]
+    Main,
+}
+
+pub type MainRouterAnchor = RouterAnchor<MainRoute>;
 
 struct Main {
     dispatch: StoreDispatchProps,
@@ -40,7 +53,21 @@ impl Component for Main {
                 <br/>
                 <button onclick=toggle>{"Store test: toggle"}</button>
                 <br/><br/>
-                <page_list::PageList/>
+                <MainRouterAnchor route=MainRoute::Main>{"Main"}</MainRouterAnchor>
+                <br/><br/>
+                <Router<MainRoute>
+                    render = Router::render(|switch: MainRoute| {
+                        match switch {
+                            MainRoute::Main => html! {<page_list::PageList/>},
+                            MainRoute::Page(id) => html! {
+                                <page::PageWithDispatch
+                                    dispatch={DispatchProps::default()}
+                                    id={id}
+                                />
+                            },
+                        }
+                    })
+                />
             </div>
         }
     }
